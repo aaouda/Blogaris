781b4358f23aca6ed972c944f07f215734f576fd
aaouda <ingenieuraaouda@gmail.com>
2016-07-26 18:23:46 +0100

    Create single.php

---
+++ single.php
@@ -0,0 +1,112 @@
+<?php get_header(); ?>
+
+<div class="row">
+				<div class="col-md-8">
+					<div class="main">
+
+            <?php if(have_posts()) : while(have_posts()) : the_post (); ?>
+						<article class="post-container">
+							<div class="post-img">
+                 <?php if (has_post_thumbnail()){
+                   The_post_thumbnail();
+                 } ?>
+							</div>
+							<div class="post-categorie">
+								<?php the_category(); ?>
+							</div>
+							<div class="post-title">
+								<h2>
+                 <a href="<?php the_permalink(); ?>">
+                   <?php the_title(); ?>
+                 </a>
+                 </h2>
+							</div>
+              <div class="post-footer">
+								<div class="post-info">
+							    <span class="recent recent-author"><i class="fa fa-user"></i> <?php the_author(); ?> </span>
+                  <span class="recent recent-time"><i class="fa fa-clock-o"></i> <?php the_time('d F, Y'); ?> </span>
+                  <span class="recent recent-comments">
+                    <i class="fa fa-comment-o"></i>
+                    <?php
+                    comments_popup_link( 'No comments yet', '1 comment', '% comments', 'comments-link', 'Comments off');
+                    ?>
+                  </span>
+								</div>
+							</div>
+							<div class="post-content">
+								<?php the_content(); ?>
+							</div>
+						</article>
+
+            <?php endwhile; else: ?>
+               <h2> post not found </h2>
+            <?php endif; ?>
+
+            <div class="comments_template">
+							<?php
+							  if(comments_open){
+									comments_template();
+								}else {
+									echo '<h2> comments are closed </h2>';
+								}
+							?>
+            </div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="sidebar">
+						<aside class="widget author">
+							<div class="author-bg">
+								<div class="author-bg__overlay"></div>
+							</div>
+							<div class="author-profil">
+								<div class="author-photo">
+									<img src="images/profile.jpg">
+								</div>
+							    <div class="author-about">
+							    	<h2> AAOUADA Mbark </h2>
+							    	<p>
+							    		A graphic designer, a web designer and developper, a friend and a legend...
+							    	</p>
+							    </div>
+							</div>
+						</aside>
+						<aside class="widget popularpost">
+							<div class="widget__title">
+								<h2> popular posts </h2>
+							</div>
+							<?php for ($i=1; $i <=4 ; $i++): ?>
+							<a href="#">
+							    <div class="widget__post">
+							    	<img src="images/image-<?= $i ?>.jpg">
+							    	<div class="widget__post-meta">
+							    		<div class="widget__post-title">Lorem ipsum dolor sit amet.</div>
+							    		<div class="widget__post-date">
+										<time>  31 Mai 2016 </time>
+							    		</div>
+							    	</div>
+							    </div>
+							</a>
+							<?php endfor; ?>
+						</aside>
+						<aside class="widget subscribe">
+							<div class="widget__title">
+								<h2> subscribe now </h2>
+							</div>
+							<div class="widget__form">
+								<p>
+									Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit.
+                                </p>
+                                <form class="subscribe-form">
+                                   <input type="email" name="email" class="subscribe-input" placeholder="Email address" autofocus>
+                                   <button type="submit" class="subscribe-submit">Subscribe</button>
+                                </form>
+							</div>
+						</aside>
+					</div>
+				</div>
+			</div>
+
+
+<?php get_footer(); ?>
